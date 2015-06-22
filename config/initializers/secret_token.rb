6123faa83abfe1343747f86236169ae217ac3b8f
Parthan SR <parthan.sr@freshdesk.com>
2015-06-22 15:10:50 +0530

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Immucal::Application.config.secret_token = '5fc63c60f6ec94b9b2d204f535e830c132f080b04c6c97180daa300ee7073992eb95deffc2f0fb3264e44d6a89c6a8eed00de12e31132209089912df49429c96'
