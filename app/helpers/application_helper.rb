34be36eec97bf480861b69e17cd7c7e358f331cc
Parthan SR <parthan.sr@freshdesk.com>
2015-07-13 16:00:32 +0530

    show count of dues in alerts if due exist across children

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,20 @@ module ApplicationHelper
 
   # returns within-30-day alerts for the current_user
   def current_alerts(user)
-    "Alerts are coming soon."
+    alert_msg = "There are no alerts"
+    child_cnt = 0
+    due_cnt = 0
+    user.children.each do |child|
+      curr_due = child.vaccinations.find_all_by_status("duenow")
+      if curr_due.count > 0
+        child_cnt += 1
+        due_cnt += curr_due.count
+      end
+    end
+    if due_cnt > 0
+      alert_msg = "You have #{pluralize(due_cnt, "due")} across #{pluralize(child_cnt, "child")}"
+    end
+    return alert_msg
   end
+
 end
